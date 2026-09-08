3dc3e43a8af8515a42d5a07dcc460c11ab5b3767
cricsri55-creator <cricsri55@gmail.com>
2026-09-08 16:33:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://aussialphas.site/"; // change to your target page
+    window.location.href = "https://primematrix.online/"; // change to your target page
   });
 
   // ✅ Just close modal when "No" is clicked
